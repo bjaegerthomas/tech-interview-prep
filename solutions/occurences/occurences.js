9232a28deac40ce63092a3909551eaa694b1db51
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-09 15:50:57 -0400

    recommented out the example input data

--- solutions/occurences/occurences.js
+++ solutions/occurences/occurences.js
@@ -1,7 +1,7 @@
-  const numbers = [1, 4, 6, 9, 7, 9, 5, 2, 8, 10, 13, 22, 17, 20, 23, 15, 18];
+ /* const numbers = [1, 4, 6, 9, 7, 9, 5, 2, 8, 10, 13, 22, 17, 20, 23, 15, 18];
     const result = oddEvenSum(numbers);
     console.log(result);
-
+*/
 
 function oddEvenSum(numbers) {
     let evenTotal = 0;
