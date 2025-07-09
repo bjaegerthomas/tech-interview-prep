58cd436752e1d14829e0da4301e6a9da494a3f18
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-09 15:42:57 -0400

    initial solution before running in node

--- solutions/occurences/README.md
+++ solutions/occurences/README.md
@@ -6,6 +6,6 @@ Write a function that takes an array of numbers and returns an object with two p
 
 ## Solution Approach
 
-1.
-2. Time Complexity:
-3. Space Complexity:
+1. Iterate over the array to determine if a value is odd or even before using the sum function to store the total value of each in a variable for each element (odd or even). Return an object with both variables stored as keys and values.
+2. Time Complexity: O(n)
+3. Space Complexity: O(n)
---
+++ solutions/occurences/occurences.js
@@ -0,0 +1,19 @@
+/*  const numbers = [1, 4, 6, 9, 7, 9, 5, 2, 8, 10, 13, 22, 17, 20, 23, 15, 18];
+    const result = oddEvenSum(numbers);
+    console.log(result);
+*/
+
+function oddEvenSum(numbers) {
+    let evenTotal = 0;
+    let oddTotal = 0;
+    for (const num of numbers) {
+        if (numbers[num]%2 == 0) {
+            evenTotal += numbers[num];
+        }
+        else {
+            oddTotal += numbers[num];
+        }
+    }
+    const finalTotals = {"Even": evenTotal, "Odd": oddTotal};
+    return finalTotals;
+}
