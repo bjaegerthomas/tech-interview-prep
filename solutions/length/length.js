c9733fdeb85a67f5e7952f3ac0d57feea671b465
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-09 10:50:28 -0400

    updated .length so it's not a function, changed for in loop (python confusion) to for of, and updated arg for split

--- solutions/length/length.js
+++ solutions/length/length.js
@@ -1,14 +1,15 @@
-/*  const phrase = "There once was a man from manhattan who never washed his hair";
+ /* const phrase = "There once was a man from manhattan who never washed his hair";
     const result = wordLength(phrase);
     console.log(result);
 */
 
+
 function wordLength(phrase) {
-    const words = phrase.split("");
+    const words = phrase.split(" ");
     const count = {};
 
-    for (const word in words) {
-        count[word] = word.length();
+    for (const word of words) {
+        count[word] = word.length;
     }
 
     return count;
