4b947d0e5dc32e762e67924480dde33615ae78d0
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-19 18:39:11 -0400

    current line of logic for anagram problem

---
+++ udemy computer science algorithms/freq_counter_valid_anagram.js
@@ -0,0 +1,22 @@
+function validAnagram(word1, word2){
+    
+    let split1 = word1.split("");
+    let split2 = word2.split("");
+    
+    if (word1.length.trim() != word2.length.trim()) {
+        console.log("These two strings are not valid anagrams of each other.")
+    }
+    
+    for (letter1 in split1) {
+        for (letter2 in split2) {
+            
+            let index1 = split1.indexOf(letter1);
+            let index2 = split2.indexOf(letter2);
+            
+            if (split1[letter1] == split2[letter2]) {
+                split1.splice(index1, 1);
+                split2.splice(index2, 1);
+            }
+        }
+    }
+  }
