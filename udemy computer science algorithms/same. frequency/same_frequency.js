6323e330c70f143e731b23f1bd7c4df8ff2d5be1
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-21 14:20:19 -0400

    changed to for of loop, changed first edge case for str, object.values

--- udemy computer science algorithms/same. frequency/same_frequency.js
+++ udemy computer science algorithms/same. frequency/same_frequency.js
@@ -1,13 +1,14 @@
 function sameFrequency(num1, num2){
-    if (num1.length !== num2.length) {
-        return false;
-    }
     let num1Str = num1.toString();
     let num2Str = num2.toString();
     let count1 = {};
     let count2 = {};
+
+    if (num1Str.length !== num2Str.length) {
+        return false;
+    }
     
-    for (const digit in num1Str) {
+    for (const digit of num1Str) {
         if (count1[digit]) {
             count1[digit] +=1;
         }
@@ -17,9 +18,9 @@ function sameFrequency(num1, num2){
         console.log(count1);
     }
 
-    let max1 = Math.max(...count1.values(count1));
+    let max1 = Math.max(...Object.values(count1));
 
-    for (const digit in num2Str) {
+    for (const digit of num2Str) {
         if (count2[digit]) {
             count2[digit] +=1;
         }
@@ -29,13 +30,14 @@ function sameFrequency(num1, num2){
         console.log(count2);
     }
 
-    let max2 = Math.max(...count2.values(count2));
+    let max2 = Math.max(...Object.values(count2));
 
     if (max1 == max2) {
         return true;
     }
     else {
         return false;
-    }
-    
+    }   
 }
+
+sameFrequency(2334, 2558);
