4d0af6506a889b3fe0c79ce61527a4176faf846c
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-21 16:22:26 -0400

    first attempt at are there duplicates problem

---
+++ udemy computer science algorithms/are there duplicates/are_there_duplicates.js
@@ -0,0 +1,15 @@
+function areThereDuplicates(arr) {
+    let i = 0;
+    
+    for (var j = 1; j < arr.length; j ++) {
+        if (arr[i] == arr[j]) {
+            return true;
+        }
+        else {
+            arr[i] = arr[j];
+        }
+    }
+    return false;
+  }
+  
+  areThereDuplicates(7, 8, 9, 10, 8, 9, 11);
