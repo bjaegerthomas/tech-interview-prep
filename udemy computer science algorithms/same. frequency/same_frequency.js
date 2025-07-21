65017c0851c442bc49f563035cf02b7f5d29e788
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-21 13:50:50 -0400

    changing tactics on same frequency

--- udemy computer science algorithms/same. frequency/same_frequency.js
+++ udemy computer science algorithms/same. frequency/same_frequency.js
@@ -1,14 +1,29 @@
-function sameFrequency(arr){
-    if (arr.length === 0) {
-        return -1;
+function sameFrequency(num1, num2){
+    if (num1.length !== num2.length) {
+        return false;
     }
-  let count1 = 0;
-  let count2 = 0;
-  let pointer1 = 0;
-  for (pointer2 = 1; pointer2 < arr.length; pointer2 ++) {
-      if (arr[pointer1] == arr[pointer2])
-  }
-  
-  
-  
+    let num1 = num1.split("");
+    let num2 = num2.split("");
+    let count1 = {};
+    let count2 = {};
+    
+    for (const digit1 in num1) {
+        if (count1[digit1]) {
+            count1[digit1] +=1;
+        }
+        else {
+            count1[digit1] = 1;
+        }
+    }
+
+    for (const digit2 in num2) {
+        if (count2[digit2]) {
+            count2[digit2] +=1;
+        }
+        else {
+            count2[digit2] = 1;
+        }
+    }
+
+    
 }
