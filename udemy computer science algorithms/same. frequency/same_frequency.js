77aab9bb90bba7b2c1dc9d735ad1308d60f0707c
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-21 14:16:17 -0400

    turned integers into strings to convert to obj for same freq

--- udemy computer science algorithms/same. frequency/same_frequency.js
+++ udemy computer science algorithms/same. frequency/same_frequency.js
@@ -2,29 +2,29 @@ function sameFrequency(num1, num2){
     if (num1.length !== num2.length) {
         return false;
     }
-    let num1Obj = num1.split("");
-    let num2Obj = num2.split("");
+    let num1Str = num1.toString();
+    let num2Str = num2.toString();
     let count1 = {};
     let count2 = {};
     
-    for (const digit1 in num1Obj) {
-        if (count1[digit1]) {
-            count1[digit1] +=1;
+    for (const digit in num1Str) {
+        if (count1[digit]) {
+            count1[digit] +=1;
         }
         else {
-            count1[digit1] = 1;
+            count1[digit] = 1;
         }
         console.log(count1);
     }
 
     let max1 = Math.max(...count1.values(count1));
 
-    for (const digit2 in num2Obj) {
-        if (count2[digit2]) {
-            count2[digit2] +=1;
+    for (const digit in num2Str) {
+        if (count2[digit]) {
+            count2[digit] +=1;
         }
         else {
-            count2[digit2] = 1;
+            count2[digit] = 1;
         }
         console.log(count2);
     }
