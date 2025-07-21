c73bfb8114ca770c37c05743545c7cf8b99aafe1
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-21 13:32:33 -0400

    started in on same frequency but rethinking logic

---
+++ udemy computer science algorithms/same. frequency/same_frequency.js
@@ -0,0 +1,14 @@
+function sameFrequency(arr){
+    if (arr.length === 0) {
+        return -1;
+    }
+  let count1 = 0;
+  let count2 = 0;
+  let pointer1 = 0;
+  for (pointer2 = 1; pointer2 < arr.length; pointer2 ++) {
+      if (arr[pointer1] == arr[pointer2])
+  }
+  
+  
+  
+}
