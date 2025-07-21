f9ca90dbf74300fe86a69a8ecca8decf6c7b5333
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-21 14:04:18 -0400

    First full attempt at same frequency before testing

--- udemy computer science algorithms/same. frequency/same_frequency.js
+++ udemy computer science algorithms/same. frequency/same_frequency.js
@@ -2,28 +2,40 @@ function sameFrequency(num1, num2){
     if (num1.length !== num2.length) {
         return false;
     }
-    let num1 = num1.split("");
-    let num2 = num2.split("");
+    let num1Obj = num1.split("");
+    let num2Obj = num2.split("");
     let count1 = {};
     let count2 = {};
     
-    for (const digit1 in num1) {
+    for (const digit1 in num1Obj) {
         if (count1[digit1]) {
             count1[digit1] +=1;
         }
         else {
             count1[digit1] = 1;
         }
+        console.log(count1);
     }
 
-    for (const digit2 in num2) {
+    let max1 = Math.max(...count1.values(count1));
+
+    for (const digit2 in num2Obj) {
         if (count2[digit2]) {
             count2[digit2] +=1;
         }
         else {
             count2[digit2] = 1;
         }
+        console.log(count2);
     }
 
+    let max2 = Math.max(...count2.values(count2));
+
+    if (max1 == max2) {
+        return true;
+    }
+    else {
+        return false;
+    }
     
 }
