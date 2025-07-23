7a74e0a08692eb664bfd5fedf90ce6a23e46f659
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-23 19:33:43 -0400

    going the helper route for power problem before checking solution

--- udemy computer science algorithms/recursion/power/power.js
+++ udemy computer science algorithms/recursion/power/power.js
@@ -1,7 +1,11 @@
 function power(){
+    
     let base = arguments[0];
     let expo = arguments[1];
-    for (let i = 0; i <= expo; i++) {
-        return base * power();
+
+    function helper(expo) {
+        for (let i = 0; i <= expo; i++) {
+            return base * helper();
+        }
     }
 }
