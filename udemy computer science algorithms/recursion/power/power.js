44fdb0a04e1d0b8162f89b9e613ae7efcc516dca
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-23 19:42:13 -0400

    some pointers from gpt

--- udemy computer science algorithms/recursion/power/power.js
+++ udemy computer science algorithms/recursion/power/power.js
@@ -3,12 +3,13 @@ function power(){
     let base = arguments[0];
     let expo = arguments[1];
 
-    function helper(expo) {
+    function helper() {
+        if (expo === 0) return 1;
         for (let i = 0; i <= expo; i++) {
-            expo --;
-            return base * helper();
+            return base * helper(expo - 1);
         }
     }
+    return helper(expo);
 }
 
 console.log(power(2,2));
