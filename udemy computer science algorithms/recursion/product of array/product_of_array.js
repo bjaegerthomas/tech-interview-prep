35c3f97a222683eba9dc07c09bd159fc242caee9
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-23 20:18:42 -0400

    first try at solution

--- udemy computer science algorithms/recursion/product of array/product_of_array.js
+++ udemy computer science algorithms/recursion/product of array/product_of_array.js
@@ -1,3 +1,6 @@
-function productOfArray() {
-    
+function productOfArray(arr) {
+    if (arr.length === 0) return 0;
+    for (i = 1; i < arr.length; i++) {
+        return arr[0] * productOfArray(arr-i);
+    }
 }
