68bb2676351d40e1008f2dd0361f978f9ea4286f
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-23 20:22:56 -0400

    had my recursion call argument incrementing in the wrong direction

--- udemy computer science algorithms/recursion/product of array/product_of_array.js
+++ udemy computer science algorithms/recursion/product of array/product_of_array.js
@@ -1,6 +1,6 @@
 function productOfArray(arr) {
     if (arr.length === 0) return 0;
     for (let i = 1; i < arr.length; i++) {
-        return arr[0] * productOfArray(arr-i);
+        return arr[0] * productOfArray(arr+i);
     }
 }
