07a8a5e92cb1b661fcad5026d946794ee3e17631
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-23 20:20:56 -0400

    forgot let in for loop for productOfArray problem

--- udemy computer science algorithms/recursion/product of array/product_of_array.js
+++ udemy computer science algorithms/recursion/product of array/product_of_array.js
@@ -1,6 +1,6 @@
 function productOfArray(arr) {
     if (arr.length === 0) return 0;
-    for (i = 1; i < arr.length; i++) {
+    for (let i = 1; i < arr.length; i++) {
         return arr[0] * productOfArray(arr-i);
     }
 }
