93b59dc0bffbf89d9083f4dc8c68faeea11506a3
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-23 20:29:44 -0400

    commented on code after gpt help

--- udemy computer science algorithms/recursion/product of array/product_of_array.js
+++ udemy computer science algorithms/recursion/product of array/product_of_array.js
@@ -1,5 +1,5 @@
 function productOfArray(arr) {
-    if (arr.length === 0) return 0;
+    if (arr.length === 0) return 0; //Was thinking this was an edge case for if someone entered an empty array from the start, but it actually needs to be my edge case
     for (let i = 1; i < arr.length; i++) {
         return arr[0] * productOfArray(arr[i]);
     }
