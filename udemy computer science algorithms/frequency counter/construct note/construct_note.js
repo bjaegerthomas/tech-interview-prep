037bcd25921523cc54824d1aa798329733761425
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-24 20:18:24 -0400

    updated construct note js file with first pass at my solution before testing

--- udemy computer science algorithms/frequency counter/construct note/construct_note.js
+++ udemy computer science algorithms/frequency counter/construct note/construct_note.js
@@ -1,3 +1,23 @@
-function constructNote(){
+function constructNote(msg, ltr) {
+    const message = msg.split("").sort();
+    const letters = ltr.splt("").sort();
     
+    let msgCount = {};
+    let ltrCount = {};
+
+    for (let letter of message) {
+        msgCount[letter] = msgCount[letter] ? msgCount[letter] + 1 : 1;
+    }
+
+    for (let letter of letters) {
+        ltrCount[letter] = ltrCount[letter] ? ltrCount[letter] + 1 : 1;
+    }
+
+    let result = [];
+
+    for (i = 0; i <= ltr.length; i++) {
+        if (ltrCount[i] === msgCount[i] && Object.values(ltrCount[i]) < Object.values(msgCount[i])) {
+            return false;
+        }
+    }
   }
