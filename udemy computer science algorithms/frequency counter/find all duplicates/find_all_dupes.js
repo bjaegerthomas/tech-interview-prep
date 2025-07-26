2c4a2d264a4266ac3a291f25eac312c7a6d6bf1f
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-26 14:10:41 -0400

    first attempt at soultion for find all dupes

--- udemy computer science algorithms/frequency counter/find all duplicates/find_all_dupes.js
+++ udemy computer science algorithms/frequency counter/find all duplicates/find_all_dupes.js
@@ -1,3 +1,13 @@
-function findAllDuplicates() {
-
+function findAllDuplicates(arr) {
+    let search = {};
+    for (let num of arr) {
+        search[num] = (search[num] || 0) +1;
+    }
+    let result = [];
+    for (let i in search) {
+        if (search[i] > 1) {
+            result.push(search[i]);
+        }
+    }
+    return result;
   }
