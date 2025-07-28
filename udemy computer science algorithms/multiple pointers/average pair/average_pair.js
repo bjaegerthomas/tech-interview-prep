1165334f5b02b46b201371b0bffb9edc5d5fb386
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-27 21:22:28 -0400

    added my first attempt for avg pair problem

--- udemy computer science algorithms/multiple pointers/average pair/average_pair.js
+++ udemy computer science algorithms/multiple pointers/average pair/average_pair.js
@@ -1,3 +1,18 @@
-function averagePair(){
-    
+function averagePair(arr, avg){
+    let left = 0;
+    let right = arr.length -1;
+    let result = [];
+
+    while (left < right) {
+        let newAvg = (left + right)/2;
+        if (newAvg == avg) {
+            result.push([right, left]);
+            right--;
+        }
+        if (newAvg < avg) {
+            left++
+        }
+        right--;
+    }
+    return result;
   }
