f3bfb969e847e3c8fa514e06ec3b2bd5f50691e8
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-29 11:50:46 -0400

    updating small syntax errors for findPair.js

--- udemy computer science algorithms/multiple pointers/findPair/findPair.js
+++ udemy computer science algorithms/multiple pointers/findPair/findPair.js
@@ -1,8 +1,8 @@
 function findPair(arr, n) {
     arr.sort();
     let left = 0;
-    let right = arr.length() - 1;
-    val = arr[right] - arr[left];
+    let right = arr.length - 1;
+    let val = arr[right] - arr[left];
 
     while (left < right) {
         if (val == n) {
