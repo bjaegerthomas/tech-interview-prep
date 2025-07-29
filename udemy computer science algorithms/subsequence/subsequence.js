dddbc233f0d557a16058a46c827a23a531147d5c
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-28 21:34:05 -0400

    solution so far for subsequence multi point problem

--- udemy computer science algorithms/subsequence/subsequence.js
+++ udemy computer science algorithms/subsequence/subsequence.js
@@ -1,3 +1,19 @@
 function isSubsequence(a, b) {
-    
-  }
+    const str1 = a.split("");
+    const str2 = b.split("");
+    let p1 = 0;
+    let p2 = 0;
+
+    while (p1 <= str1.length - 1) {
+        if (p2 > str2.length - 1) {
+            return false;
+        }
+        else if (str1[p1] === str2[p2]) {
+            p1++;
+            p2++;
+        }
+        else {
+            p2++;
+        }
+    }
+}
