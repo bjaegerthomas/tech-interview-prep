54b16af0bf4f312c8dbc79a835bfd76baac54c98
jaegerthomas <bjaegerthomas@gmail.com>
2025-07-31 11:11:23 -0400

    updated readme and minSubarrayLen.js with proposed solution

--- udemy computer science algorithms/sliding windows/minSubarrayLen/minSubarrayLen.js
+++ udemy computer science algorithms/sliding windows/minSubarrayLen/minSubarrayLen.js
@@ -1,3 +1,24 @@
 function minSubarrayLen (arr, num) {
-    
+    if (arr == []) {
+        return null;
+    }
+    let newSum = 0;
+    let minLen = 0;
+    while (newSum < sum) {
+        newSum = newSum + arr[minLen];
+        if (minLen > arr.length) {
+            return 0;
+        }
+        minLen++;
+    }
+    let newLen = minLen;
+    for (let i = newLen; newLen > arr.length; i++) {
+        newSum = newSum - arr[i - num] + arr[i];
+        if (newSum >= sum) {
+            if (i < newLen) {
+                minLen = i;
+            }
+        }
+    }
+    return minLen;
 }
