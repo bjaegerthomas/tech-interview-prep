be3369f21a09e3844ff9179a79cd4b89b1ed241b
jaegerthomas <bjaegerthomas@gmail.com>
2025-08-05 10:11:10 -0400

    first pass at possible logic and solution using countZeroes for sorted frequency problem

--- udemy computer science algorithms/sliding windows/sorted sequence/README.md
+++ udemy computer science algorithms/sliding windows/sorted sequence/README.md
@@ -4,6 +4,7 @@
 Given a sorted array and a number, write a function called sortedFrequency that counts the occurrences of the number in the array.
 
 ## Possible Logic
+I'm going to take the same approach as the findZeroes problem but replace 0 with the inputed number variable. Just copying the code over and tweaking.
 
 ## Given Solution
 
--- udemy computer science algorithms/sliding windows/sorted sequence/sortedSequence.js
+++ udemy computer science algorithms/sliding windows/sorted sequence/sortedSequence.js
@@ -1,3 +1,20 @@
-function sortedFrequency(){
-    // add whatever parameters you deem necessary - good luck!
-  }
+function sortedFrequency(arr, num){
+        
+    let firstNum = findFirst(arr, num)
+    if (firstNum === -1) return 0;
+    
+    return arr.length - firstNum
+    }
+       
+function findFirst(arr, num, low = 0, high = arr.length - 1) {
+    if (high >= low) {
+        let mid = low + Math.floor((high - low) / 2)
+        if ((mid === num || arr[mid - 1] !== num) && arr[mid] === num) {
+        return mid;
+        } else if (arr[mid] !== num) {
+        return findFirst(arr, num, mid + 1, high)
+        }
+        return findFirst(arr, num, low, mid - 1)
+    }
+    return -1;
+}
