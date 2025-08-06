5ec6638de2eca16c718ec2ca98c651f5187576fd
jaegerthomas <bjaegerthomas@gmail.com>
2025-08-06 12:33:39 -0400

    small adjustment to naiveSearch.js

--- udemy computer science algorithms/search/naiveStringSearch/naiveSearch.js
+++ udemy computer science algorithms/search/naiveStringSearch/naiveSearch.js
@@ -1,9 +1,10 @@
 function naiveSearch(long, short){
     let count = 0;
-    for (let i = 0; i < long; i++) {
+    for (let i = 0; i < long.length; i++) {
         let j = i;
         while (short[j] === long[j]) {
             if (j === short.length - 1) count++;
+            else j++;
         }
    }
    return count;
