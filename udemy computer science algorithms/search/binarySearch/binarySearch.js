7d981f43c24017c5098dd7bcc3a00c7879a49ea2
jaegerthomas <bjaegerthomas@gmail.com>
2025-08-06 09:59:16 -0400

    two mistakes in logic, determining mid and updating left and right for binarySearch

--- udemy computer science algorithms/search/binarySearch/binarySearch.js
+++ udemy computer science algorithms/search/binarySearch/binarySearch.js
@@ -3,10 +3,10 @@ function binarySearch(arr, num){
     let right = arr.length - 1;
 
     while (left < right) {
-        let mid = Math.floor((right - left)/2)
+        let mid = Math.floor((right + left)/2)
         if (arr[mid] === num) return mid;
-        else if (arr[mid] < num) left++;
-        else right--;
+        else if (arr[mid] < num) left = mid + 1;
+        else right = mid - 1;
     }
     return -1;
   }
