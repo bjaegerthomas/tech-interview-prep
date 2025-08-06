7f6af7aa2836824d6e288dd3d197d5df1492ec90
jaegerthomas <bjaegerthomas@gmail.com>
2025-08-06 09:53:44 -0400

    first pass at solution for binary search

--- udemy computer science algorithms/search/binarySearch/binarySearch.js
+++ udemy computer science algorithms/search/binarySearch/binarySearch.js
@@ -1,3 +1,12 @@
-function binarySearch(){
-    // add whatever parameters you deem necessary - good luck!
+function binarySearch(arr, num){
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((right - left)/2)
+        if (arr[mid] === num) return mid;
+        else if (arr[mid] < num) left++;
+        else right--;
+    }
+    return -1;
   }
