a6fa238d0c3dbf080dbe98fc07e421255a5b4504
jaegerthomas <bjaegerthomas@gmail.com>
2025-08-15 14:48:56 -0400

    updated with given solution for insert in singlyLinkedList

--- udemy computer science algorithms/data_structures/singly linked lists/singlyLinkedList.js
+++ udemy computer science algorithms/data_structures/singly linked lists/singlyLinkedList.js
@@ -82,13 +82,15 @@ class SinglyLinkedList{
         return false;
     }
     insert(index, val){
-        var newNode = new Node(val);
         if(index < 0 || index > this.length) return false;
-        if(index === this.length) this.push(newNode);
-        if(index === 0) this.unshift(newNode);
-        var oldNode = this.get(index - 1);
-        oldNode.next = newNode;
-        newNode.next = oldNode;
+        if(index === this.length) return !!this.push(val);
+        if(index === 0) return !!this.unshift(val);
+        
+        var newNode = new Node(val);
+        var prev = this.get(index - 1);
+        var temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
         this.length++;
         return true;
     }
