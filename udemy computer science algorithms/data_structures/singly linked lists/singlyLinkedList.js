168fa6cb9d665f266da9ea9866fa7f7a75ab5a95
jaegerthomas <bjaegerthomas@gmail.com>
2025-08-15 11:36:11 -0400

    updated singlyLinkedList with given push method

--- udemy computer science algorithms/data_structures/singly linked lists/singlyLinkedList.js
+++ udemy computer science algorithms/data_structures/singly linked lists/singlyLinkedList.js
@@ -1,6 +1,3 @@
-// piece of data - val
-//reference to next node - next
-
 class Node{
     constructor(val){
         this.val = val;
@@ -15,14 +12,15 @@ class SinglyLinkedList{
         this.length = 0;
     }
     push(val){
-        if (this.length === 0) {
-            this.head = val;
-            this.tail = val;
-            this.length++;
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
         }
-        else {
-            this.tail = val;
-            this.length++;
-        } 
+        this.length++;
+        return this;
     }
 }
