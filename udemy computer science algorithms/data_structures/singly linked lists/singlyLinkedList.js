de9ba04ae27336fab0be6108dc6f7b4457a507ac
jaegerthomas <bjaegerthomas@gmail.com>
2025-08-15 13:33:27 -0400

    attempt an unshift solution for singlyLinkedList

--- udemy computer science algorithms/data_structures/singly linked lists/singlyLinkedList.js
+++ udemy computer science algorithms/data_structures/singly linked lists/singlyLinkedList.js
@@ -50,4 +50,17 @@ class SinglyLinkedList{
         }
         return currentHead;
     }
+    unshift(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        else{
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
