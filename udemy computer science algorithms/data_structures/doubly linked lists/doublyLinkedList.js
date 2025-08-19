1961ca23b362e47916ad0806a68734cabfb45405
jaegerthomas <bjaegerthomas@gmail.com>
2025-08-19 15:06:19 -0400

    updated with given solution for insert for DLL

--- udemy computer science algorithms/data_structures/doubly linked lists/doublyLinkedList.js
+++ udemy computer science algorithms/data_structures/doubly linked lists/doublyLinkedList.js
@@ -99,16 +99,15 @@ class DoublyLinkedList{
     }
     insert(index, val){
         if(index < 0 || index > this.length) return false;
-        if(index === this.length) return !!this.push(val);
         if(index === 0) return !!this.unshift(val);
-        
+        if(index === this.length) return !!this.push(val);
+
         var newNode = new Node(val);
-        var prev = this.get(index - 1);
-        var temp = prev.next;
-        prev.next = newNode;
-        newNode.next = temp;
-        temp.prev = newNode;
-        newNode.prev = prev;
+        var beforeNode = this.get(index-1);
+        var afterNode = beforeNode.next;
+        
+        beforeNode.next = newNode, newNode.prev = beforeNode;
+        newNode.next = afterNode, afterNode.prev = newNode;
         this.length++;
         return true;
     }
