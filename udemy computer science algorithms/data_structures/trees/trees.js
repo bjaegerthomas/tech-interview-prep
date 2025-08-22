85adcc04198b5d503ca06ee976391ae76fa50a12
jaegerthomas <bjaegerthomas@gmail.com>
2025-08-22 13:55:17 -0400

    partial attempt at solution for BST

--- udemy computer science algorithms/data_structures/trees/trees.js
+++ udemy computer science algorithms/data_structures/trees/trees.js
@@ -1,12 +1,26 @@
-class BinarySearchTree {
-    constructor(){
-        this.root = null;
-    }
-}
 class Node {
     constructor(value){
         this.value = value;
         this.left = null;
         this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        var newNode = new Node(value);
+        var tree = new BinarySearchTree();
+        if (!root){
+            this.root = newNode;
+        }
+        else if (value < this.root && !this.left){
+            this.left = newNode;
+        }
+        else if (value > this.rott && !this.right){
+            this.right = newNode;
+        }     
     }
 }
