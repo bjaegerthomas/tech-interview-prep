be8a682fcf556dd16ddfa403592a0d11074a0986
jaegerthomas <bjaegerthomas@gmail.com>
2025-08-29 10:32:48 -0400

    first attempt at find method for BST

--- udemy computer science algorithms/data_structures/trees/trees.js
+++ udemy computer science algorithms/data_structures/trees/trees.js
@@ -34,4 +34,24 @@ class BinarySearchTree {
             }
         }
     }
+    find(value){
+        var current = this.root;
+        var exists = false;
+        if(this.root === null) return false;
+        while(exists === false){
+            if(value === current.value) exists = true;
+            if(value < current.value){
+                if(current.left === null){
+                    break;
+                }
+                current = current.left;
+            } else {
+                if(current.right === null){
+                    break;
+                } 
+                current = current.right;
+            }
+        }
+        return exists;
+    }
 }
