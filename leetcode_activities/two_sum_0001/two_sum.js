db9e3cd021dae21b010a99f991fcb7440f504abc
jaegerthomas <bjaegerthomas@gmail.com>
2025-09-02 15:35:25 -0400

    updated solution after test run

--- leetcode_activities/two_sum_0001/two_sum.js
+++ leetcode_activities/two_sum_0001/two_sum.js
@@ -1,10 +1,9 @@
-function twoSum(nums,target){
+function twoSum(nums, target) {
     let answer = [];
-    for (let i = 0; i < nums.length; i++){
-        for (let k = 1; k < nums.length; k++){
-            answer = [nums[i], nums[k]];
-            if (nums[i] + nums[k] === target) break;
+    for (let i = 0; i < nums.length; i++) {
+        for (let k = 1; k < nums.length; k++) {
+            answer = [i, k];
+            if (nums[i] + nums[k] === target) return answer;
         }
     }
-    return answer;
 }
