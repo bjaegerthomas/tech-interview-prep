5e535d2f135ced40baf2787c9d312119d9cf3ab0
jaegerthomas <bjaegerthomas@gmail.com>
2025-09-02 12:54:26 -0400

    attempt at post order dfs on trees

--- udemy computer science algorithms/data_structures/trees/trees.js
+++ udemy computer science algorithms/data_structures/trees/trees.js
@@ -108,4 +108,14 @@ class BinarySearchTree {
         traverse(this.root);
         return data;
     }
+    DFSPostOrder(){
+        var data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
 }
