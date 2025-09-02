134d5f04e345c9c631b94d7d6e6c7560375b9d50
jaegerthomas <bjaegerthomas@gmail.com>
2025-09-02 11:03:06 -0400

    updated trees.js with breadth first search

--- udemy computer science algorithms/data_structures/trees/trees.js
+++ udemy computer science algorithms/data_structures/trees/trees.js
@@ -84,4 +84,18 @@ class BinarySearchTree {
         last = current;
         return current;
     }
+    BFS(){
+        var node = this.root,
+            data = [],
+            queue = [];
+        queue.push(node);
+
+        while(queue.length){
+           node = queue.shift();
+           data.push(node.value);
+           if(node.left) queue.push(node.left);
+           if(node.right) queue.push(node.right);
+        }
+        return data;
+    }
 }
