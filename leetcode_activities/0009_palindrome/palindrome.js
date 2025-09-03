da406655cfd8199dfacac0a32e697e48b85500eb
jaegerthomas <bjaegerthomas@gmail.com>
2025-09-03 16:00:32 -0400

    third attempt at plaidrome leetcode

--- leetcode_activities/0009_palindrome/palindrome.js
+++ leetcode_activities/0009_palindrome/palindrome.js
@@ -1,12 +1,14 @@
 // Create a function named palindrome which accepts an integer
 function palindrome(x) {
-    //create new variable for integer and convert to string
+    // create new variable for integer and convert to string
     let numString = x.toString();
+    // if length of integer is only 1 digit return true
+    if (numString.length === 1) return true;
     // initialize variable right for right side of integer to 0
     let right = 0;
         // for loop which runs through length of integer - 1 where i starts at x - 1 and decrements every pass as long as i
         // is > x.length/2
-        for (let i = numString.length - 1; i > numString.length/2; i--) {
+        for (let i = numString.length - 1; i >= numString.length/2; i--) {
             // check if right and i !==
             // if true return false
             if (numString[right] !== numString[i]) return false;
