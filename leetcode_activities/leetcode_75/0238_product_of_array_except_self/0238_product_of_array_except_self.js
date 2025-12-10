6b368f410630f22a8749033288ae5e45ac94a3c6
jaegerthomas <bjaegerthomas@gmail.com>
2025-12-10 13:54:44 -0500

    second attempt but not in O(n) time

--- leetcode_activities/leetcode_75/0238_product_of_array_except_self/0238_product_of_array_except_self.js
+++ leetcode_activities/leetcode_75/0238_product_of_array_except_self/0238_product_of_array_except_self.js
@@ -1,3 +1,4 @@
+/*
 //define function which accepts an array of integers nums: function productOfArrExceptSelf (nums)
 function productOfArrExceptSelf (nums) {
     //define variable total: const total = nums.reduce((acc, num) => acc + num, 0)
@@ -8,6 +9,27 @@ function productOfArrExceptSelf (nums) {
     for (let i = 0; i < nums.length; i++) {
         //subtract current nums value from the total and push it to the result array: result.push(total - nums[i])
         result.push(total - nums[i]);
+    }
+    //return the result: return result
+    return result;
+}
+*/
+
+//define function which accepts an array of integers nums: function productOfArrExceptSelf (nums)
+function productOfArrExceptSelf (nums) {
+    //define resulting array: let result = []
+    let result = [];
+    //define current product: let currProd = 1
+    let currProd = 1;
+    //loop through nums array: for (let i = 0; i < nums.length; i++)
+    for (let i = 0; i < nums.length; i++) {
+        //loop through nums array: for (let k = 0; k < nums.length; k++)
+        for (let k = 0; k < nums.length; k++) {
+            //check to see you aren't using the current i value of the nums array to tabulate your product: if (k !== i) currProd *= nums[k]
+            if (k !== i) currProd *= nums[k];
+        }
+        //push currProd to result array: result.push(currProd)
+        result.push(currProd);
     }
     //return the result: return result
     return result;
--- leetcode_activities/leetcode_75/0238_product_of_array_except_self/README.md
+++ leetcode_activities/leetcode_75/0238_product_of_array_except_self/README.md
@@ -31,6 +31,18 @@ The input is generated such that answer[i] is guaranteed to fit in a 32-bit inte
     //loop through array nums: for (let i = 0; i < nums.length; i++)
         //subtract current nums value from the total and push it to the result array: result.push(total - nums[i])
     //return the result: return result
+
+ATTEMPT #2 with the correct problem parameters but obviously not in O(n) time:
+
+//define function which accepts an array of integers nums: function productOfArrExceptSelf (nums)
+    //define resulting array: let result = []
+    //define current total: let currTotal = 1
+    //loop through nums array: for (let i = 0; i < nums.length; i++)
+        //loop through nums array: for (let k = 0; k < nums.length; k++)
+            //check to see you aren't using the current i value of the nums array to tabulate your product: if (k !== i) currTotal *= nums[k]
+        //push currTotal to result array: result.push(currTotal)
+    //return the result: return result
+
    
 # Given Solution
 
