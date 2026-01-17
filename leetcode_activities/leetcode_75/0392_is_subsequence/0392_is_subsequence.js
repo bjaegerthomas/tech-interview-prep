e66424b2fc112658ed597bf8eb694a6ae443efa9
jaegerthomas <bjaegerthomas@gmail.com>
2026-01-17 12:14:21 -0500

    first attempt

---
+++ leetcode_activities/leetcode_75/0392_is_subsequence/0392_is_subsequence.js
@@ -0,0 +1,16 @@
+//define function which accepts two strings, s and t: function isSubsequence (s,t)
+function isSubsequence (s,t) {
+    //define left pointer: let left = 0
+    let left = 0;
+    //define right pointer and loop through array: for (let right = 0; right < t.length; right++)
+    for (let right = 0; right < t.length; right++) {
+        //check if current value in s is equal to the current value in t: if (s[left] == t[right])
+            //if they are equal increment left: left++
+        if (s[left] == t[right]) left++;
+    }
+    //check to see if the left pointer made it all the way through the string: if (left == s.length - 1)
+        //if it's true return true: return true
+    if (left == s.length - 1) return true;
+    //otherwise return false: return false
+    return false;
+}
