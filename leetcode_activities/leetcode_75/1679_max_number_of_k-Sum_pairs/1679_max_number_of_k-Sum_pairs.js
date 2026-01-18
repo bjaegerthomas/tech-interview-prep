2e1a6c5d0860c3645813a3f69d058e438cf1fd03
jaegerthomas <bjaegerthomas@gmail.com>
2026-01-18 11:12:37 -0500

    updated with first attempt at solution

---
+++ leetcode_activities/leetcode_75/1679_max_number_of_k-Sum_pairs/1679_max_number_of_k-Sum_pairs.js
@@ -0,0 +1,27 @@
+//define a function which accepts an array nums and an integer k: function maxOperations (nums, k)
+function maxOperations (nums, k) {
+    //define left pointer: let lp = 0
+    let lp = 0;
+    //define right pointer: let rp = nums.length - 1
+    let rp = nums.length - 1;
+    //define operations counter: maxOp = 0
+    maxOp = 0;
+    //loop through array: while (lp < rp)
+    while (lp < rp) {
+        //check if current values add up to k: if (nums[lp] + nums[rp] == k)
+        if (nums[lp] + nums[rp] == k) {
+            //if true increase operations counter: maxOp++
+            maxOp++;
+            //remove value at index lp from array: nums.slice(lp,1)
+            nums.slice(lp,1);
+            //remove value at index rp from array: nums.slice(rp,1)
+            nums.slice(rp,1);
+            //reset rp value: rp = nums.length - 1
+            rp = nums.length - 1;
+        }
+        //if it doesn't add up decrement rp and check again: else rp--
+        else rp--;
+    }
+    //return the maximum number o operations: return maxOp
+    return maxOp;
+}
--- leetcode_activities/leetcode_75/1679_max_number_of_k-Sum_pairs/README.md
+++ leetcode_activities/leetcode_75/1679_max_number_of_k-Sum_pairs/README.md
@@ -31,6 +31,18 @@ Constraints:
 1 <= k <= 10^9
 
 # Approach
+//define a function which accepts an array nums and an integer k: function maxOperations (nums, k)
+    //define left pointer: let lp = 0
+    //define right pointer: let rp = nums.length - 1
+    //define operations counter: maxOp = 0
+    //loop through array: while (lp < rp)
+        //check if current values add up to k: if (nums[lp] + nums[rp] == k)
+            //if true increase operations counter: maxOp++
+            //remove value at index lp from array: nums.slice(lp,1)
+            //remove value at index rp from array: nums.slice(rp,1)
+            //reset rp value: rp = nums.length - 1
+        //if it doesn't add up decrement rp and check again: else rp--
+    //return the maximum number o operations: return maxOp
 
 # Given Solution
 
